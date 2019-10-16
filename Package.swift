e594f0b5f861c4f316ec623f1f214e423daea60a
CypherPoet <CypherPoet@gmail.com>
2019-10-16 16:24:52 -0500

    Experiment with implicit path

--- Package.swift
+++ Package.swift
@@ -30,13 +30,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PreviewUtils",
-            dependencies: [],
-            path: "Sources/PreviewUtils/"
+            dependencies: []
         ),
         .target(
             name: "ViewModifiers",
-            dependencies: [],
-            path: "Sources/ViewModifiers/"
+            dependencies: []
         )
     ]
 )
