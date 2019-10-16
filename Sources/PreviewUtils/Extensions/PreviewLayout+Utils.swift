39fad8540e60a8b5640d58d4bbd5b53e32dd0ab6
CypherPoet <CypherPoet@gmail.com>
2019-10-16 15:53:01 -0500

    Add `iPadPro11InchLandscape` to preview layout extensions

--- Sources/PreviewUtils/Extensions/PreviewLayout+Utils.swift
+++ Sources/PreviewUtils/Extensions/PreviewLayout+Utils.swift
@@ -3,7 +3,16 @@ import SwiftUI
 
 
 /// Helpers for manually simulating landscape-mode previews, since this is not yet supported directly
+/// Reference: https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/adaptivity-and-layout/
 extension PreviewLayout {
+    
+    // iPhone
+    
     public static var iPhoneSELandscape: PreviewLayout = .fixed(width: 568, height: 320)
     public static var iPhone11Landscape: PreviewLayout = .fixed(width: 896, height: 414)
+    
+    
+    // iPad
+    
+    public static var iPadPro11InchLandscape: PreviewLayout = .fixed(width: 2388, height: 1668)
 }
