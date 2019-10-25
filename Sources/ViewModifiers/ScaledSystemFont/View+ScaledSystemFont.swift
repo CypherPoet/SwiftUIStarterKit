2cc8b53e2edb37ceb009f84b907ffacc0a7d0252
CypherPoet <CypherPoet@gmail.com>
2019-10-24 19:11:40 -0500

    Fix typo

--- Sources/ViewModifiers/ScaledSystemFont/View+ScaledSystemFont.swift
+++ Sources/ViewModifiers/ScaledSystemFont/View+ScaledSystemFont.swift
@@ -5,9 +5,7 @@ import SwiftUI
 @available(iOS 13, macCatalyst 13, tvOS 13, watchOS 6, *)
 extension View {
     
-    public func scaledSystemFont(
-        size: CGFloat,
-    ) -> some View {
+    public func scaledSystemFont(size: CGFloat) -> some View {
         self.modifier(ScaledSystemFont(size: size))
     }
 }
