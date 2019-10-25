45b7bed096f0cc77082dc2d4c762aecfd29a0717
CypherPoet <CypherPoet@gmail.com>
2019-10-24 19:14:52 -0500

    Revert to using more options for system font

--- Sources/ViewModifiers/ScaledSystemFont/ScaledSystemFont.swift
+++ Sources/ViewModifiers/ScaledSystemFont/ScaledSystemFont.swift
@@ -9,11 +9,15 @@ public struct ScaledSystemFont: ViewModifier {
     /// the system know that it needs to reload the `body` view when changes occur.
     @Environment(\.sizeCategory) var sizeCategory
     
+    
     public var size: CGFloat
+    public var weight: Font.Weight = .regular
+    public var design: Font.Design = .default
+
     
     public func body(content: Content) -> some View {
        let scaledSize = UIFontMetrics.default.scaledValue(for: size)
         
-        return content.font(.system(size: size))
+        return content.font(.system(size: size, weight: weight, design: design))
     }
 }
--- Sources/ViewModifiers/ScaledSystemFont/View+ScaledSystemFont.swift
+++ Sources/ViewModifiers/ScaledSystemFont/View+ScaledSystemFont.swift
@@ -5,7 +5,11 @@ import SwiftUI
 @available(iOS 13, macCatalyst 13, tvOS 13, watchOS 6, *)
 extension View {
     
-    public func scaledSystemFont(size: CGFloat) -> some View {
-        self.modifier(ScaledSystemFont(size: size))
+    public func scaledSystemFont(
+        size: CGFloat,
+        weight: Font.Weight = .regular,
+        design: Font.Design = .default
+    ) -> some View {
+        self.modifier(ScaledSystemFont(size: size, weight: weight, design: design))
     }
 }
