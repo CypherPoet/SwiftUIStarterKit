cfe7b0435a175503de67f855ba6bb5c79cf6f297
CypherPoet <CypherPoet@gmail.com>
2019-11-01 02:56:42 -0500

    Add public init to reducer

--- Sources/DataFlowUtils/Reducer.swift
+++ Sources/DataFlowUtils/Reducer.swift
@@ -10,4 +10,9 @@ import Foundation
 
 public struct Reducer<State, Action> {
     public let reduce: (inout State, Action) -> Void
+    
+    
+    public init(reduce: @escaping (inout State, Action) -> Void) {
+        self.reduce = reduce
+    }
 }
