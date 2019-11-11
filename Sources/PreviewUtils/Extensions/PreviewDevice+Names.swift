470533a4bbf96135c63505c093e33e494df5f40a
CypherPoet <CypherPoet@gmail.com>
2019-11-11 04:31:15 -0600

    Add iPhone11ProMax preview device name, fix `iPhone11Pro`

--- Sources/PreviewUtils/Extensions/PreviewDevice+Names.swift
+++ Sources/PreviewUtils/Extensions/PreviewDevice+Names.swift
@@ -5,6 +5,7 @@ extension PreviewDevice {
     public enum Name {
         public static let iPhone11 = "iPhone 11"
         public static let iPhone11Pro = "iPhone 11 Pro"
+        public static let iPhone11ProMax = "iPhone 11 Pro Max"
     }
 }
 
@@ -13,5 +14,6 @@ extension PreviewDevice {
 extension PreviewDevice {
     
     public static let iPhone11 = PreviewDevice(rawValue: PreviewDevice.Name.iPhone11)
-    public static let iPhone11Pro = PreviewDevice(rawValue: PreviewDevice.Name.iPhone11)
+    public static let iPhone11Pro = PreviewDevice(rawValue: PreviewDevice.Name.iPhone11Pro)
+    public static let iPhone11ProMax = PreviewDevice(rawValue: PreviewDevice.Name.iPhone11ProMax)
 }
