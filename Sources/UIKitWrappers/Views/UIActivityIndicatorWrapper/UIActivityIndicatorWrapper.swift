25824ffef2440758425fe667beb06d534ec5a5d4
CypherPoet <CypherPoet@gmail.com>
2020-04-05 08:30:42 -0500

    Change UIActivityIndicatorWrapper to ActivityIndicatorViewWrapper and update API.

--- Sources/UIKitWrappers/Views/UIActivityIndicatorWrapper/UIActivityIndicatorWrapper.swift
+++ Sources/UIKitWrappers/Views/UIActivityIndicatorWrapper/UIActivityIndicatorWrapper.swift
@@ -1,47 +1,61 @@
 import SwiftUI
-import UIKit
 
 
-public struct UIActivityIndicatorWrapper {
-    public typealias UIViewType = UIActivityIndicatorView
+struct ActivityIndicatorViewWrapper {
+    typealias UIViewType = UIActivityIndicatorView
     
-    public var color: UIColor
-    public var backgroundColor: UIColor
-    public var indicatorStyle: UIActivityIndicatorView.Style
-    
+    var isAnimating: Bool
+    var color: UIColor
+    var backgroundColor: UIColor
+    var indicatorStyle: UIActivityIndicatorView.Style
+    var hidesWhenStopped: Bool
 
-    public init(
+    
+    init(
+        isAnimating: Bool,
         color: UIColor = .systemGray,
         backgroundColor: UIColor = .clear,
-        indicatorStyle: UIActivityIndicatorView.Style = .large
+        indicatorStyle: UIActivityIndicatorView.Style = .large,
+        hidesWhenStopped: Bool = true
     ) {
+        self.isAnimating = isAnimating
         self.color = color
         self.backgroundColor = backgroundColor
         self.indicatorStyle = indicatorStyle
+        self.hidesWhenStopped = hidesWhenStopped
     }
 }
 
 
 // MARK: - UIViewRepresentable
-extension UIActivityIndicatorWrapper: UIViewRepresentable {
+extension ActivityIndicatorViewWrapper: UIViewRepresentable {
 
-    public func makeUIView(
-        context: UIViewRepresentableContext<UIActivityIndicatorWrapper>
-    ) -> UIViewType {
-        let activityIndicator = UIActivityIndicatorView(style: indicatorStyle)
-        
-        activityIndicator.sizeToFit()
-        activityIndicator.color = color
-        activityIndicator.backgroundColor = backgroundColor
+    // Treat the `makeUIView` method as `init(frame: CGRect)` in a normal UIKit `UIView`.
+    func makeUIView(context: Context) -> UIViewType {
+        let activityIndicator = UIActivityIndicatorView()
+
+        configure(activityIndicator)
         
         return activityIndicator
     }
 
 
-    public func updateUIView(
-        _ activityIndicator: UIViewType,
-        context: UIViewRepresentableContext<UIActivityIndicatorWrapper>
-    ) {
-        activityIndicator.startAnimating()
+    func updateUIView(_ activityIndicator: UIViewType, context: Context) {
+        configure(activityIndicator)
+        
+        isAnimating ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
+    }
+}
+
+
+// MARK: - View Configuration
+extension ActivityIndicatorViewWrapper {
+    
+    func configure(_ activityIndicator: UIActivityIndicatorView) {
+        activityIndicator.sizeToFit()
+        activityIndicator.style = indicatorStyle
+        activityIndicator.color = color
+        activityIndicator.backgroundColor = backgroundColor
+        activityIndicator.hidesWhenStopped = hidesWhenStopped
     }
 }
