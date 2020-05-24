45248a7c9e644ed5bbaecd6e405f6ffb72f1397d
CypherPoet <CypherPoet@gmail.com>
2020-05-23 19:09:16 -0500

    Add `all` helper for PreviewDevices

--- Sources/PreviewUtils/Extensions/PreviewDevice+Names.swift
+++ Sources/PreviewUtils/Extensions/PreviewDevice+Names.swift
@@ -2,14 +2,14 @@ import SwiftUI
 
 
 extension PreviewDevice {
-    
+
     public enum Name {
         public static let iPhone8 = "iPhone 8"
         public static let iPhone8Plus = "iPhone 8 Plus"
         public static let iPhone11 = "iPhone 11"
         public static let iPhone11Pro = "iPhone 11 Pro"
         public static let iPhone11ProMax = "iPhone 11 Pro Max"
-        
+
         public static let iPadPro9Point7Inch = "iPad Pro (9.7-inch)"
         public static let iPadPro11Inch = "iPad Pro (11-inch)"
     }
@@ -23,7 +23,17 @@ extension PreviewDevice {
     public static let iPhone11 = PreviewDevice(rawValue: PreviewDevice.Name.iPhone11)
     public static let iPhone11Pro = PreviewDevice(rawValue: PreviewDevice.Name.iPhone11Pro)
     public static let iPhone11ProMax = PreviewDevice(rawValue: PreviewDevice.Name.iPhone11ProMax)
-    
+
     public static let iPadPro9Point7Inch = PreviewDevice(rawValue: PreviewDevice.Name.iPadPro9Point7Inch)
     public static let iPadPro11Inch = PreviewDevice(rawValue: PreviewDevice.Name.iPadPro11Inch)
+
+    public static let all: [PreviewDevice] = [
+        .iPhone8,
+        .iPhone8Plus,
+        .iPhone11,
+        .iPhone11Pro,
+        .iPhone11ProMax,
+        .iPadPro9Point7Inch,
+        .iPadPro11Inch,
+    ]
 }
--- Sources/PreviewUtils/Extensions/PreviewLayout+Utils.swift
+++ Sources/PreviewUtils/Extensions/PreviewLayout+Utils.swift
@@ -1,18 +1,15 @@
-import Foundation
 import SwiftUI
 
 
 /// Helpers for manually simulating landscape-mode previews, since this is not yet supported directly
 /// Reference: https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/adaptivity-and-layout/
 extension PreviewLayout {
-    
-    // iPhone
-    
+
+    // MARK: - iPhone
     public static var iPhoneSELandscape: PreviewLayout = .fixed(width: 568, height: 320)
     public static var iPhone11Landscape: PreviewLayout = .fixed(width: 896, height: 414)
-    
-    
-    // iPad
-    
+
+
+    // MARK: - iPad
     public static var iPadPro11InchLandscape: PreviewLayout = .fixed(width: 2388, height: 1668)
 }
