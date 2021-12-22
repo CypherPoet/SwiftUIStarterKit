dfd83fcd18e02f0c4beb4de0b7f4452cf88eac65
CypherPoet <CypherPoet@gmail.com>
2021-12-21 21:47:34 -0600

    Move `SwiftBarcodeScanningUtils` into separate package.

--- Sources/SwiftUIStarterKit/PreferenceKeys/FrameSizePreferenceKey.swift
+++ Sources/SwiftUIStarterKit/PreferenceKeys/FrameSizePreferenceKey.swift
@@ -4,14 +4,18 @@ import SwiftUI
 public struct FrameSizePreferenceKey: PreferenceKey {
     public static let defaultValue = CGSize.zero
     
-    public static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+    public static func reduce(
+        value: inout CGSize, nextValue: () -> CGSize
+    ) {
     }
 }
 
 
 extension View {
     
-    public func readingFrameSize(_ newSizeHandler: @escaping (CGSize) -> Void) -> some View {
+    public func readingFrameSize(
+        _ newSizeHandler: @escaping (CGSize) -> Void
+    ) -> some View {
         background(
             GeometryReader { geometry in
                 Color.clear.preference(key: FrameSizePreferenceKey.self, value: geometry.size)
@@ -25,5 +29,13 @@ extension View {
                 }
             }
         )
+    }
+}
+
+
+struct FrameSizePreferenceKey_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
